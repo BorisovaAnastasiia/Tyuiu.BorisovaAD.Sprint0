e2f1dd21b1447b759139268a1f52fdd6a2f70db7
Anastasiia Borisova <kuqhoxp@gmail.com>
2025-09-22 01:36:06 +0500

    Task5

---
+++ Tyuiu.BorisovaAD.Sprint0.Task5.V0.Test/DataServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using Tyuiu.BorisovaAD.Sprint0.Task5.V0.Lib;
+namespace Tyuiu.BorisovaAD.Sprint0.Task5.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedAdditionValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+        public void CheckedSubtractionValid()
+        {
+            Assert.AreEqual(20, DataService.Subtraction(100, 80));
+        }
+
+        public void CheckedMultiplicationValid()
+        {
+            Assert.AreEqual(27, DataService.Multiplication(9, 3));
+        }
+
+        public void CheckedDivisionValid()
+        {
+            Assert.AreEqual(9, DataService.Division(72, 8));
+        }
+    }
+}
