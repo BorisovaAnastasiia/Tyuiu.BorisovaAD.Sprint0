697c4afb5b8b83447f430fb5b99b665ff4b1dad6
Anastasiia Borisova <kuqhoxp@gmail.com>
2025-09-21 23:00:55 +0500

    Task2

---
+++ Tyuiu.BorisovaAD.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,9 @@
+﻿using Tyuiu.BorisovaAD.Sprint0.Task2.V0.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(DataService.GetMessage("Анастасия"));
+        Console.ReadKey();
+    }
+}
